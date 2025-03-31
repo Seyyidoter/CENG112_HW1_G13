41af28a351c2d07fe04ddfc324bf7b86afb2e323
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-03-31 12:08:14 +0300

    Updated

--- src/data/TreasureBox.java
+++ src/data/TreasureBox.java
@@ -2,17 +2,7 @@ package data;
 import model.QuestCard;
 
 public class TreasureBox extends Bag<QuestCard>{
-
     public TreasureBox(){
         super();
     }
-
-    /*
-    public int calculateValue(){
-        int total = 0;
-        // will be coded.
-    }
-    */
-
-
 }
