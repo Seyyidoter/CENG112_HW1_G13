afa37f1858b08ad84e7be9843f6b9c35e507dc88
Emir Kağan Yiğit <67961133+emirkaganyigit@users.noreply.github.com>
2025-04-01 22:01:36 +0300

    Update GameApp.java

--- src/game/GameApp.java
+++ src/game/GameApp.java
@@ -1,9 +1,13 @@
 package game;
 
+//gameapp starts the game
 public class GameApp {
     public static void main(String[] args) {
+        //create new game object
         Game game = new Game();
+        //initialize the game with name
         game.initializeGameComponents("Seyyid");
+        //start the game
         game.play();
     }
 }
