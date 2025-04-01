1d5049c1991b48fd89172eb8906b5c84e2dafae3
Emir Kağan Yiğit <67961133+emirkaganyigit@users.noreply.github.com>
2025-04-01 21:51:06 +0300

    Update Tent.java

--- src/data/Tent.java
+++ src/data/Tent.java
@@ -1,8 +1,10 @@
 package data;
 import model.Treasure;
 
+//tent will store the treasures which are earned by player
 public class Tent extends Bag<Treasure>{
+    //constructor
     public Tent(){   
-        super();
+        super(); //call bag's constructor to create empty tent
     }
 }
