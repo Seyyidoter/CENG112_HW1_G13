87b3e6f8cdc588ed3063b92826f1bcca272c8363
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-04-01 22:31:19 +0300

    Updated

--- src/model/Treasure.java
+++ src/model/Treasure.java
@@ -1,23 +1,23 @@
 package model;
 
-//abstract class for all treasures
+//Abstract class for all treasures
 public abstract class Treasure {
 
-    //each card will have a value
+    //Each card will have a value
     private int value;
 
-    //constructor 
+    //Constructor 
     public Treasure(int value){
         this.value = value;
     }
 
-    // returns the treasure type and its value 
+    //Returns the treasure type and its value 
     @Override
     public String toString(){
         return this.getClass().getSimpleName() + " (Value: " + value + ")";
     }
 
-    // getter for value
+    //Getter for value
     public int getValue(){
         return this.value;
     }
