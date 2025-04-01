b9c3c88792000461337e293afe36e0eb526fe646
Emir Kağan Yiğit <67961133+emirkaganyigit@users.noreply.github.com>
2025-04-01 22:16:53 +0300

    Update Turquoise.java

--- src/model/Turquoise.java
+++ src/model/Turquoise.java
@@ -1,7 +1,8 @@
 package model;
 
+//turquoise is a treasure card
 public class Turquoise extends Treasure{
     public Turquoise() {
-        super(1);
+        super(1); ////constructor sets the value
     }
 }
