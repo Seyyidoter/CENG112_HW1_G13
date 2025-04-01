811c7e63a25cb2cc43f3e5bf497480d5f3fd9c4b
Emir Kağan Yiğit <67961133+emirkaganyigit@users.noreply.github.com>
2025-04-01 21:48:56 +0300

    Update HazardBox.java

--- src/data/HazardBox.java
+++ src/data/HazardBox.java
@@ -1,10 +1,12 @@
 package data;
 import model.QuestCard;
 
+//hazardbox will store drawn hazard cards
 public class HazardBox extends Bag<QuestCard>{
 
+    //constructor
     public HazardBox(){
-        super();
+        super(); //use bag's constructor to create empty box
     }
 
 }
