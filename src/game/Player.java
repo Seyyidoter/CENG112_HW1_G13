b694dd08c7b27fdadd8fdcd08e62dcd7141e6a7c
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-04-01 22:28:35 +0300

    Updated

--- src/game/Player.java
+++ src/game/Player.java
@@ -7,44 +7,44 @@ import model.Treasure;
 
 public class Player {
 
-    //player's features
+    //Player's features
     private String name; 
     private int score;
     private Tent tent;
     private TreasureBox treasureBox;
     private HazardBox hazardBox;
 
-    //constructor
+    //Constructor
     public Player(String name){
         this.name = name;
         this.score = 0;
-        // create an empty tent, tresureBox and hazardBox
+        //Create an empty tent, tresureBox and hazardBox
         this.tent = new Tent();
         this.treasureBox = new TreasureBox(); 
         this.hazardBox = new HazardBox(); 
     }
 
-    //getter for player name
+    //Getter for player name
     public String getName(){
         return name;
     }
 
-    //getter for score
+    //Getter for score
     public int getScore(){
         return score;
     }
 
-    //this method gives a number from the interval of 0-29
-    //we will use it to choose random card
+    //This method gives a number from the interval of 0-29
+    //We will use it to choose random card
     public int rollDice(){
         return (int) (Math.random() * 30);
     }
 
-    //calculate the score with considering items in tent
+    //Calculate the score with considering items in tent
     public int calculateScore() {
         int totalScore = 0;
 
-        //until tent is empty pick every treasure and add its value to totalscore
+        //Until tent is empty pick every treasure and add its value to totalscore
         while (!tent.isEmpty()) {
             Treasure treasure = tent.remove();
             totalScore += treasure.getValue();
@@ -59,7 +59,7 @@ public class Player {
         return "Player: " + name + " Score: " + score;
     }
 
-    //getter for tent, treasurebox, hazardbox
+    //Getter for tent, treasurebox, hazardbox
     public Tent getTent() {
         return tent;
     }
