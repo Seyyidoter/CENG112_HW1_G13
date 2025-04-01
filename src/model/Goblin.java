01f0fc9e08e67a950537e926508a8fcba57bbe68
Emir Kağan Yiğit <67961133+emirkaganyigit@users.noreply.github.com>
2025-04-01 22:06:41 +0300

    Update Goblin.java

--- src/model/Goblin.java
+++ src/model/Goblin.java
@@ -1,7 +1,8 @@
 package model;
 
+//goblin is a hazard card
 public class Goblin extends HazardCard{
     public Goblin(){
-        super("Goblin");
+        super("Goblin"); //constructor sets the name
     }
 }
