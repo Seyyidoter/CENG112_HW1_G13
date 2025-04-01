61bc5b39f695f7b41f292f52c5fcc604dececc17
Emir Kağan Yiğit <67961133+emirkaganyigit@users.noreply.github.com>
2025-04-01 22:11:12 +0300

    Update Spider.java

--- src/model/Spider.java
+++ src/model/Spider.java
@@ -1,7 +1,8 @@
 package model;
 
+//spider is a hazard card
 public class Spider extends HazardCard{
     public Spider(){
-        super("Spider");
+        super("Spider"); //constructor sets the name
     }
 }
