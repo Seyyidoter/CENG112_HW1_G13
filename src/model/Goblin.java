18c36c541b6241286b3de4bcb44744e868585ef3
Seyyid Öter <150018480+Seyyidoter@users.noreply.github.com>
2025-04-01 22:29:11 +0300

    Updated

--- src/model/Goblin.java
+++ src/model/Goblin.java
@@ -1,8 +1,8 @@
 package model;
 
-//goblin is a hazard card
+//Goblin is a hazard card
 public class Goblin extends HazardCard{
     public Goblin(){
-        super("Goblin"); //constructor sets the name
+        super("Goblin"); //Constructor sets the name
     }
 }
